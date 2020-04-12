6adeccd91f55922e8d6f91ed3b07985380d4a26c
Jacob Mampane Laptop <Jacob.Mampane@Liberty.co.za>
2020-04-12 22:29:16 +0200

    rsa_bond_ed failing to produce output

--- rsa_bond_ed.py
+++ rsa_bond_ed.py
@@ -2,15 +2,9 @@ from rsa_bond import rsab
 from rsab_fwd import rsab_fwd
 from datetime import datetime
 
-today = datetime(datetime.today().year, datetime.today().month, datetime.today().day, 12, 0, 0)  # clunky
 
-
-def rsa_bond_ed(bond=None, forward_yield=0.10, number_of_units=0, transaction_date=today, trader=None, repo_rate=0.00,
-                far_maturity_date=None, trade_yield_or_price=0.00, bank_broker=None):
-
-    yyyy = far_maturity_date.strftime("%Y")
-    mm = far_maturity_date.strftime("%m")
-    dd = far_maturity_date.strftime("%d")
+def rsa_bond_ed(bond=None, forward_yield=0.10, number_of_units=0, trader=None, repo_rate=0.00, far_maturity_date=None,
+                trade_yield_or_price=0.00, bank_broker=None):
 
     # ALM MRM facing externaly, booked into custody account: Spot
     book = 'A:LG:ALM MRM ANN AND GCB:GOVBND:U'
@@ -39,12 +33,20 @@ def rsa_bond_ed(bond=None, forward_yield=0.10, number_of_units=0, transaction_da
     ed_int_fwd_tkt = rsab_fwd(bond, forward_yield, -number_of_units, trader, repo_rate, far_maturity_date, book,
                               counterparty)
 
-    mkt_ext_spot_tkt.to_excel(f'{bond}_ext_spot.xlsx')
-    mkt_int_spot_tkt.to_excel(f'{bond}_int_spot_mkt.xlsx')
-    trs_int_spot_tkt.to_excel(f'{bond}_int_spot_trs.xlsx')
-    trs_int_fwd_tkt.to_excel(f'{bond}_int_fwd_trs.xlsx')
-    ed_int_fwd_tkt.to_excel(f'{bond}_int_fwd_ed.xlsx')
+    # mkt_ext_spot_tkt.to_excel(f'test_{bond}_ext_spot.xlsx')
+    # mkt_int_spot_tkt.to_excel(f'test_{bond}_int_spot_mkt.xlsx')
+    # trs_int_spot_tkt.to_excel(f'test_{bond}_int_spot_trs.xlsx')
+    # trs_int_fwd_tkt.to_excel(f'test_{bond}_int_fwd_trs.xlsx')
+    # ed_int_fwd_tkt.to_excel(f'test_{bond}_int_fwd_ed.xlsx')
 
+    return mkt_ext_spot_tkt
+
+
+m_date = datetime(2020, 6, 17, 18, 0, 0)
+tkt = rsa_bond_ed(bond='R186', trade_yield_or_price=0.1, forward_yield=0.102, number_of_units=100, trader='Jacob',
+                  repo_rate=0.0560, far_maturity_date=m_date, bank_broker='SBBJ')
+
+print(tkt)
 
 
 
