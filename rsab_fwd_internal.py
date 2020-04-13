cf5d49441b9bb4d12f44f4f73d1fb4633ffdac7d
Jacob Mampane Laptop <Jacob.Mampane@Liberty.co.za>
2020-04-13 12:21:04 +0200

    excel output added to function, name changed

--- rsab_fwd_internal.py
+++ rsab_fwd_internal.py
@@ -2,8 +2,14 @@ from rsab_fwd import rsab_fwd
 import pandas as pd
 
 
-def rsab_fwd_int(bond=None, forward_yield=None, number_of_units=None, transaction_date=None, trader=None,
-                 repo_rate=None, far_maturity_date=None, mirror_desk=None):
+def rsabfwd_internal(bond=None, forward_yield=None, number_of_units=None, transaction_date=None, trader=None,
+                     repo_rate=None, far_maturity_date=None, mirror_desk=None):
+
+    yyyy = far_maturity_date.strftime("%Y")
+    mm = far_maturity_date.strftime("%m")
+    dd = far_maturity_date.strftime("%d")
+
+    trade_ref = f'{yyyy}{mm}{dd}_{bond}_BNDFWD'
 
     d = {'Counterparty Desk': ['GIT', 'ED'],
          'Counterparty Name': ['Libfin GIT', 'Liberty Libfin Emdedded Derivs R'],
@@ -23,6 +29,9 @@ def rsab_fwd_int(bond=None, forward_yield=None, number_of_units=None, transactio
     price_taker = rsab_fwd(bond=bond, forward_yield=forward_yield, number_of_units=-number_of_units,
                            transaction_date=transaction_date, trader=trader, repo_rate=repo_rate,
                            counterparty='Libfin Treasury', book=book, far_maturity_date=far_maturity_date)
+
+    price_maker.to_excel(f'{trade_ref}_{mirror_desk}.xlsx', header=False)
+    price_taker.to_excel(f'{trade_ref}_Treasury.xlsx', header=False)
 
     return price_maker, price_taker
 
