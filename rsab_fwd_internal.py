e447b195c5da044035206df81ff368b5afe8b34c
Jacob Mampane Laptop <Jacob.Mampane@Liberty.co.za>
2020-04-13 13:23:21 +0200

    added dictionary to allow testing of function, along with the first untested output files

--- rsab_fwd_internal.py
+++ rsab_fwd_internal.py
@@ -1,9 +1,10 @@
 from rsab_fwd import rsab_fwd
 import pandas as pd
+from datetime import datetime
 
 
 def rsabfwd_internal(bond=None, forward_yield=None, number_of_units=None, transaction_date=None, trader=None,
-                     repo_rate=None, far_maturity_date=None, mirror_desk=None):
+                     repo_rate=None, far_maturity_date=None, mirror_desk=None, create_ticket=True):
 
     yyyy = far_maturity_date.strftime("%Y")
     mm = far_maturity_date.strftime("%m")
@@ -30,9 +31,18 @@ def rsabfwd_internal(bond=None, forward_yield=None, number_of_units=None, transa
                            transaction_date=transaction_date, trader=trader, repo_rate=repo_rate,
                            counterparty='Libfin Treasury', book=book, far_maturity_date=far_maturity_date)
 
-    price_maker.to_excel(f'{trade_ref}_{mirror_desk}.xlsx', header=False)
-    price_taker.to_excel(f'{trade_ref}_Treasury.xlsx', header=False)
+    if create_ticket:
+        price_maker.to_excel(f'{trade_ref}_{mirror_desk}.xlsx', header=False)
+        price_taker.to_excel(f'{trade_ref}_Treasury.xlsx', header=False)
 
-    return price_maker, price_taker
+    else:
+        return price_maker, price_taker
 
-# Not tested
+
+# Not tested as I am unable to open Alchemy
+
+test_trade = {'bond': 'R2048', 'forward_yield':  0.1002, 'number_of_units': 100,
+              'transaction_date': datetime(2020, 4, 9, 12, 0, 0), 'trader': 'Nico Nchabeleng', 'repo_rate': 0.0560,
+              'far_maturity_date': datetime(2020, 6, 17, 18, 0, 0), 'mirror_desk': 'GIT', 'create_ticket': True
+              }
+rsabfwd_internal(**test_trade)
